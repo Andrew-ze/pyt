a0f611340a88fe71ffd63e1830e3f5134cf2efab
Andrew-ze <drewsalim9@gmail.com>
2026-07-01 13:11:25 +0300

    first commit

---
+++ Desktop/Python prog/scratch.py
@@ -0,0 +1,44 @@
+#import decimal
+#integer = 15
+#print(decimal.Decimal(integer))
+#print(type(decimal.Decimal(integer)))
+
+#import decimal
+#string = "12345"
+#print(decimal.Decimal(string))
+#print(type(decimal.Decimal(string)))
+
+#string = "Python Programming"
+#print(string[::-1])
+
+#Python program to display calender
+#import calendar
+#year = int(input("Enter year: "))
+#month = int(input("Enter month: "))
+
+#cal = calendar.month(year, month)
+#print(cal)
+
+#A python program to solve a quadratic equation.
+#import math
+
+#input coefficients
+#a = float(input("Enter coefficient a: "))
+#b = float(input("Enter coefficient b: "))
+#c = float(input("Enter corfficient c: "))
+
+#discriminant = b**2 - 4*a*c
+#if discriminant > 0:
+    #root1 = (-b + math.sqrt(discriminant)) / (2*a)
+    #root2 = (-b - math.sqrt(discriminant)) / (2*a)
+    #print(f"Root 1: {root1}")
+    #print(f"Root 2: {root2}")
+#elif discriminant == 0:
+    #root = -b / (2*a)
+    #print(f"Root: {root}") 
+#else:
+    #real_part = -b / (2*a)
+    #imaginary_part = math.sqrt(abs(discriminant)) / (2*a)
+    #print(f"Root 1: {real_part} + {imaginary_part}i")
+    #print(f"Root2: {real_part} - {imaginary_part}i")
+
